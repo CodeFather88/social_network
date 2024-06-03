747b4981b08207571d990636921baf26a5636715
Igor-Web-Developer <bulanovigor01@gmail.com>
2024-06-03 14:53:43 +0300

    socket swagger

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,15 +1,72 @@
-// import { Body, Controller, Post } from '@nestjs/common';
-// import { ApiOperation } from '@nestjs/swagger';
-// import { CurrentUser } from '@shared/decorators';
-// import { JwtPayload } from 'jsonwebtoken';
+import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CreateChatDto } from './dto/createChat.dto';
 
-// @Controller('chat')
-// export class ChatController {
-//     constructor() { }
+@ApiTags('Chats')
+@Controller('chat')
+export class ChatController {
+    constructor() {
+      const a = 'd'
+  }
+
+  @ApiOperation({
+    summary: 'WebSocket Events',
+    description: `
+### События WebSocket:
+
+#### \`createChat\`
+
+    Событие, отправляемое клиентом при создании чата.
+    ример использования:
+
     
-//     @ApiOperation({ summary: 'Create chat (for USER)' })
-//     @Post('create')
-//     async createChat(@CurrentUser() user: JwtPayload, @Body() dto: CreateChatDto) {
-        
-//     }
-// }
+    const createChatObject = {
+        name: 'Test Chat', // Название чата, который будет создан
+        addedUsers: ['dd834c8a-b67d-4326-905a-101115ebfb3b'] // массив id пользователей, приглашаемых в чат, который будет создан
+    };
+    socket.emit('createChat', createChatObject);
+   
+#### \`.\`
+
+#### \`createChatResponse\`
+
+    Событие, которое должно прослушиваться клиентом при создании чата
+    Пример использования:
+
+    socket.on('createChatResponse', (data) => {
+        console.log('Chat created:', data.result);
+      });
+
+
+
+
+#### \`.\`
+#### \`.\`
+#### \`.\`
+#### \`.\`
+#### \`.\`
+#### \`.\`
+#### \`.\`
+#### \`.\`
+#### \`.\`
+#### \`.\`
+#### \`.\`
+#### \`.\`
+
+
+
+
+#### \`newMessage\`
+
+    Событие, отправляемое при получении нового сообщения в чате.
+
+#### \`userJoined\`
+
+    Событие, отправляемое при присоединении нового пользователя к чату.
+`,
+  })
+  @Get('events')
+  async getWebSocketEvents(): Promise<string> {
+    return 'List of WebSocket events';
+  }
+}
--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -5,7 +5,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { options } from '@auth/config';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from '@auth/guards/auth.guard';
-// import { ChatController } from './chat.controller';
+import { ChatController } from './chat.controller';
 
 @Module({
   providers: [ChatGateway, ChatService, 
@@ -15,6 +15,6 @@ import { AuthGuard } from '@auth/guards/auth.guard';
     }
   ],
   imports: [JwtModule.registerAsync(options())],
-  // controllers: [ChatController]
+  controllers: [ChatController]
 })
 export class ChatModule {}
